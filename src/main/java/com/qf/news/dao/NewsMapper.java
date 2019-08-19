58956a2631d0aabf6d6a4679cbc4570d6c8f5be9
tc1134399226 <1134399226@qq.com>
2019-08-19 14:13:15 +0800

    Initial commit

---
+++ src/main/java/com/qf/news/dao/NewsMapper.java
@@ -0,0 +1,9 @@
+package com.qf.news.dao;
+
+import com.qf.news.pojo.Test;
+
+import java.util.List;
+
+public interface NewsMapper {
+    public List<Test> testMethod();
+}
---
+++ src/main/java/com/qf/news/pojo/Test.java
@@ -0,0 +1,8 @@
+package com.qf.news.pojo;
+
+import lombok.Data;
+
+@Data
+public class Test {
+    private int id;
+}
