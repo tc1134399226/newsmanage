a3a2d0f274df5c7ba2c21e5166d77c67eeea1a40
tc1134399226 <1134399226@qq.com>
2019-08-21 10:05:06 +0800

    超级管理员 操作分配后台用户权限

--- src/main/java/com/qf/sysuser/service/impl/UserServiceImpl.java
+++ src/main/java/com/qf/sysuser/service/impl/UserServiceImpl.java
@@ -26,43 +26,72 @@ public class UserServiceImpl implements SysUserService {
 //        }
 //    }
 
+    /**
+     * 获取所有用户信息 操作中间表
+     * @return
+     */
     public List<User> getAllUser() {
         return sysUserDao.getAllUser();
     }
 
+    /**
+     * 删除用户信息 操作中间表
+     * @param userid
+     * @return
+     */
     public boolean deleteUser(Integer userid) {
         int i = sysUserDao.deleteUser(userid);
         return i > 0;
     }
 
-    public boolean registerUser(User user) {
-        if (user.getUsername()==null||"".equals(user.getUsername())||user.getAccount()==null
-        ||"".equals(user.getAccount())){
-            return false;
-        }
-        User user1 = sysUserDao.getUserByuserName(user);
-        if (user1!=null){
-            return false;
-        }else {
-            int i = sysUserDao.saveUser(user);
-            return i > 0;
-        }
-    }
+//    public boolean registerUser(User user) {
+//        if (user.getUsername()==null||"".equals(user.getUsername())||user.getAccount()==null
+//        ||"".equals(user.getAccount())){
+//            return false;
+//        }
+//        User user1 = sysUserDao.getUserByuserName(user);
+//        if (user1!=null){
+//            return false;
+//        }else {
+//            int i = sysUserDao.saveUser(user);
+//            return i > 0;
+//        }
+//    }
 
+    /**
+     * 通过用户id获取用户信息
+    * @param user
+     * @return
+     */
     public User getUserById(User user) {
         return sysUserDao.getUserById(user);
     }
 
+    /**
+     * 更新后台用户信息 通过id
+     * @param user
+     * @return
+     */
     public boolean updateUser(User user) {
         int i = sysUserDao.updateUser(user);
         return i > 0;
     }
 
+    /**
+     * 通过账号或用户名模糊查询用户的信息
+     * @param user
+     * @return
+     */
     @Override
     public List<User> selectData(User user) {
         return sysUserDao.selectData(user);
     }
 
+    /**
+     * 删除(更改状态)用户
+     * @param userIdsDTO
+     * @return
+     */
     public boolean deleteUser(UserIdsDTO userIdsDTO){
         for (Integer userid : userIdsDTO.getUserids()) {
             System.out.println(userid);
@@ -70,14 +99,25 @@ public class UserServiceImpl implements SysUserService {
         int i = sysUserDao.deleteCheckUser(userIdsDTO);
         return i > 0;
     }
+
+    /**
+     * 新增后台用户
+     * @param user
+     * @return
+     */
     public boolean insertUser(User user){
         int i = sysUserDao.insertUser(user);
         return i > 0;
     }
 
 
-    public List<MenuInfo> userLoginInit(User user) {
-        return sysUserDao.userLoginInit(user);
-    }
+//    /**
+//     *  初始化用户菜单
+//     * @param user
+//     * @return
+//     */
+//    public List<MenuInfo> userLoginInit(User user) {
+//        return sysUserDao.userLoginInit(user);
+//    }
 
 }
