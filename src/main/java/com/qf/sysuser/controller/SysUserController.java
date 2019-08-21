b4e11dc57f6c50b3cadab9b1aa3901df5e443ee9
tc1134399226 <1134399226@qq.com>
2019-08-21 20:18:40 +0800

    首页

--- src/main/java/com/qf/sysuser/controller/SysUserController.java
+++ src/main/java/com/qf/sysuser/controller/SysUserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-@RequestMapping("/sysuser")
+@RequestMapping("admin/sysuser")
 public class SysUserController {
 
     @Autowired
