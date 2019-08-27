9f1326a3891e83a635b83868a1e77cf28887a7a4
userqht <1664324029@qq.com>
2019-08-27 21:46:24 +0800

    3张图

--- src/main/java/com/qf/sysuser/service/impl/SysUserArticleCarouselServiceImpl.java
+++ src/main/java/com/qf/sysuser/service/impl/SysUserArticleCarouselServiceImpl.java
@@ -1,6 +1,6 @@
 package com.qf.sysuser.service.impl;
 
-import com.qf.sysuser.dao.SysUserArticleCarouselDao;
+import com.qf.sysuser.dao.SysUserArticleCarouselMapper;
 import com.qf.sysuser.pojo.ArticleCarousel;
 import com.qf.sysuser.pojo.ArticleInfo;
 import com.qf.sysuser.service.SysUserArticleCarouselService;
@@ -13,41 +13,41 @@ import java.util.List;
 public class SysUserArticleCarouselServiceImpl implements SysUserArticleCarouselService {
 
     @Autowired
-    SysUserArticleCarouselDao sysUserArticleCarouselDao;
+    SysUserArticleCarouselMapper sysUserArticleCarouselMapper;
 
 
     //根据封面图筛选新闻
     public List<ArticleInfo> getArticleByCover(ArticleInfo articleInfo) {
-        return sysUserArticleCarouselDao.getArticleByCover(articleInfo);
+        return sysUserArticleCarouselMapper.getArticleByCover(articleInfo);
     }
 
     //筛选的新闻添加到轮播库
     public boolean addToCarousel(ArticleInfo articleInfo) {
-        return sysUserArticleCarouselDao.addToCarousel(articleInfo)>0;
+        return sysUserArticleCarouselMapper.addToCarousel(articleInfo)>0;
     }
 
     //已加入轮播的新闻将设置状态为1
     public boolean updateArticleState(int articleId) {
-        return sysUserArticleCarouselDao.updateArticleState(articleId)>0;
+        return sysUserArticleCarouselMapper.updateArticleState(articleId)>0;
     }
 
             //查询所有轮播新闻信息
             public List<ArticleCarousel> getAllArticleCarousel() {
-                return sysUserArticleCarouselDao.getAllArticleCarousel();
+                return sysUserArticleCarouselMapper.getAllArticleCarousel();
             }
 
             //查询最早添加的新闻id
             public ArticleCarousel selectArticleId() {
-                return sysUserArticleCarouselDao.selectArticleId();
+                return sysUserArticleCarouselMapper.selectArticleId();
             }
 
             //根据这个id删除掉这条新闻信息
             public boolean deleteArticleUseId(int articleId) {
-                return sysUserArticleCarouselDao.deleteArticleUseId(articleId)>0;
+                return sysUserArticleCarouselMapper.deleteArticleUseId(articleId)>0;
             }
 
             //根据id查询轮播新闻详情信息
             public ArticleCarousel getArticleCarouselById(int articleId) {
-                return sysUserArticleCarouselDao.getArticleCarouselById(articleId);
+                return sysUserArticleCarouselMapper.getArticleCarouselById(articleId);
     }
 }
--- src/main/java/com/qf/sysuser/service/impl/SysUserReportServiceImpl.java
+++ src/main/java/com/qf/sysuser/service/impl/SysUserReportServiceImpl.java
@@ -1,6 +1,6 @@
 package com.qf.sysuser.service.impl;
 
-import com.qf.sysuser.dao.SysUserReportDao;
+import com.qf.sysuser.dao.SysUserReportMapper;
 import com.qf.sysuser.service.SysUserReportService;
 import com.qf.sysuser.vo.ReportVO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +12,10 @@ import java.util.List;
 public class SysUserReportServiceImpl implements SysUserReportService {
 
     @Autowired
-    SysUserReportDao sysUserReportDao;
+    SysUserReportMapper sysUserReportMapper;
 
     //查询所有被举报的文章
     public List<ReportVO> getAllReport() {
-        return sysUserReportDao.getAllReport();
+        return sysUserReportMapper.getAllReport();
     }
 }
